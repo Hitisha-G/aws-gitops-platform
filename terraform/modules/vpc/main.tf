ed9eec97b186c57a3ef968b676c79a7aee6cdb84
Hitishaa <89304585+Hitisha-G@users.noreply.github.com>
2026-09-23 14:18:07 +0000

    Refactor public default route onto standalone aws_route

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -80,11 +80,6 @@ resource "aws_subnet" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-public-rt"
   })
@@ -95,6 +90,12 @@ resource "aws_route_table_association" "public" {
 
   subnet_id      = aws_subnet.public[each.key].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public_default" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
 }
 
 resource "aws_route_table" "private" {
